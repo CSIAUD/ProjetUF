6335ed28e88570043b6b8287610fc02db5c5d0c7
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-03-03 14:57:57 +0100

    add header / footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "landing", pathMatch: "full"},
+  { path: "team", component: LandingComponent },
+  { path: "installations", component: LandingComponent },
+  { path: "prestations", component: LandingComponent },
+  { path: "philo", component: LandingComponent },
+  { path: "withus", component: LandingComponent },
+  { path: "contact", component: LandingComponent },
+  { path: "landing", component: LandingComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 64],
+      useHash: true // [x, y]
+    })
+    // RouterModule.forRoot(routes, { useHash: true }),
+    // RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
