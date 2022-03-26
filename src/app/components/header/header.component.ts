b10b089aa05770402af41a72bd9fd8ac6fde2985
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-03-26 21:38:24 +0100

    :white_check_mark:
    initBurger
    toggleMenu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,5 +11,44 @@ export class HeaderComponent implements OnInit {
   constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+    initBurger();
   }
 }
+
+function initBurger(){
+  let burgerBase = document.getElementById("burger");
+  let menuLinks = document.querySelectorAll("#menu a");
+  let burger;
+
+  burgerBase?.addEventListener('click', function (e) {
+    if((e.target as SVGElement).ownerSVGElement){
+      burger = ((e.target as SVGElement).ownerSVGElement as SVGElement);
+    }else{
+      burger = (e.target as SVGElement);
+    }
+    toggleMenu(burger)
+  }, false);
+
+  menuLinks.forEach(link => {
+      link.addEventListener('click', ev=>{
+          let burger = ((document.getElementById("burger") as unknown) as SVGElement)
+          toggleMenu(burger)
+      })
+  });
+};
+
+function toggleMenu(burger: SVGElement) {
+  let status = burger.dataset['status']
+  if(status == "close"){
+      burger.dataset['status'] = "open";
+      
+      ((document.getElementById("stage1") as unknown) as SVGAnimateTransformElement).beginElement()
+      document.getElementById("menu")?.classList.replace("hidden",'flex')
+      document.getElementsByTagName("html")[0].style.overflow = "hidden"
+  }else{
+      burger.dataset['status'] = "close";
+      ((document.getElementById("stage2") as unknown) as SVGAnimateTransformElement).beginElement()
+      document.getElementById("menu")?.classList.replace('flex',"hidden")
+      document.getElementsByTagName("html")[0].style.overflow = "unset"
+  }
+};
